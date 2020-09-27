7decba12ee240e9c16624f985edbdaa07faf59ca
dedane <evanskeema@gmail.com>
2020-09-27 20:36:13 +0300

    'dashboards'

---
+++ src/pages/Dashboards/Farmer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Farmer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/Dashboards/Investor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Investor() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Investor
